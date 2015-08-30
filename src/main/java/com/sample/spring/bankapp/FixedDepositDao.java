75e2374bb8fe78d943e02f805e58f390164a725e
porop <porop2ya@gmail.com>
2015-08-30 16:08:40 +0900

    Bankapp/ Add bank app (chapter.1)

---
+++ src/main/java/com/sample/spring/bankapp/FixedDepositDao.java
@@ -0,0 +1,46 @@
+package com.sample.spring.bankapp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+@SuppressWarnings("unused")
+public class FixedDepositDao {
+	private static Logger logger = Logger.getLogger(FixedDepositDao.class);
+	private String url;
+	private String driverClass;
+	private String username;
+	private String password;
+
+	private Map<Long, FixedDepositDetails> fixedDeposits = new HashMap<Long, FixedDepositDetails>();
+
+	public FixedDepositDao() {
+		logger.info("initializing");
+	}
+	
+	public FixedDepositDetails getFixedDepositDetails(long id) {
+		return fixedDeposits.get(id);
+	}
+
+	public boolean createFixedDeposit(FixedDepositDetails details) {
+		this.fixedDeposits.put(details.getId(), details);
+		return true;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setDriverClass(String driverClass) {
+		this.driverClass = driverClass;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
